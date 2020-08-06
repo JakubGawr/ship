bc5c72f8cdf9a0b21062440f1090ea2b62d66e7e
jakub-gawronski <jakub.gawronski@instapage.com>
2020-08-06 22:33:47 +0200

    basic mongo connection

--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -3,14 +3,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { USER, USER_TOKEN } from '@ship-game/backend/schema';
 import { from, Observable } from 'rxjs';
+import { UserLoginDTO } from '@ship-game/backend/validation';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(USER_TOKEN) private readonly user: Model<USER>) {
   }
 
-  loginUser({name}: any): Observable<USER>{
-   const newUser = new this.user({ name });
+  loginUser(user: UserLoginDTO): Observable<UserLoginDTO>{
+   const newUser = new this.user(user);
    return from(newUser.save());
   }
 }
