8f26cf93d0036b6c031698e69499eb8768789eab
jakub-gawronski <jakub.gawronski@instapage.com>
2020-08-09 01:45:23 +0200

    :)

--- libs/frontend/dashboard/src/lib/components/sidebar-item/sidebar-item.component.ts
+++ libs/frontend/dashboard/src/lib/components/sidebar-item/sidebar-item.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+} from '@angular/core';
 import { Nav } from '../../containers/sidebar/sidebar.component';
-import { trigger, transition, style, animate, state } from '@angular/animations';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'ui-sidebar-item',
@@ -9,40 +13,28 @@ import { trigger, transition, style, animate, state } from '@angular/animations'
   animations: [
     trigger('collapseItem', [
       transition(':enter', [
-        style({transform: 'scale(0)'}),
-        animate('400ms 100ms', style({transform: 'scale(1)'})),
+        style({ transform: 'scale(0)' }),
+        animate('400ms 100ms', style({ transform: 'scale(1)' }))
       ]),
       transition(':leave', [
-        animate('300ms 100ms', style({transform: 'scale(0)'}))
+        animate('300ms 100ms', style({ transform: 'scale(0)' }))
       ])
     ]),
     trigger('collapseItem2', [
       transition(':enter', [
-        style({transform: 'scale(0)'}),
-        animate('300ms 300ms', style({transform: 'scale(1)'})),
+        style({ transform: 'scale(0)' }),
+        animate('300ms 300ms', style({ transform: 'scale(1)' }))
       ]),
       transition(':leave', [
-        animate('100ms', style({transform: 'scale(0)'}))
+        animate('100ms', style({ transform: 'scale(0)' }))
       ])
-    ]),
+    ])
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SidebarItemComponent implements OnInit {
+export class SidebarItemComponent {
   @Input() sidebarItem: Nav;
   @Input() isCollapsed: boolean;
-
-  @Output() isActive = new EventEmitter<boolean>();
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-
-  }
-
-  emitActive(){
-    this.isActive.emit(true);
-  }
-
+  @Input() standardView = true;
+  isActive = false;
 }
--- libs/frontend/dashboard/src/lib/containers/sidebar/sidebar.component.ts
+++ libs/frontend/dashboard/src/lib/containers/sidebar/sidebar.component.ts
@@ -1,10 +1,21 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
-import { trigger, state, style, transition, animateChild, group, query, animate } from '@angular/animations'
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  ViewChildren,
+  QueryList,
+  AfterViewInit, OnDestroy
+} from '@angular/core';
+import { trigger, state, style, transition, animateChild, group, query, animate } from '@angular/animations';
+import { SidebarItemComponent } from '../../components/sidebar-item/sidebar-item.component';
+import { filter, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, Route, Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 export interface Nav {
   path: string,
   name: string,
-  active: boolean,
   icon: string,
 }
 
@@ -13,22 +24,22 @@ export interface Nav {
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   animations: [
-    trigger('collapseText',[
+    trigger('collapseText', [
       transition(':leave', [
-        animate('300ms 100ms', style({transform: 'scale(0)'}))
+        animate('300ms 100ms', style({ transform: 'scale(0)' }))
       ]),
       transition(':enter', [
-        style({transform: 'scale(0)'}),
-        animate('400ms 100ms', style({transform: 'scale(1)'}))
+        style({ transform: 'scale(0)' }),
+        animate('400ms 100ms', style({ transform: 'scale(1)' }))
       ])
     ]),
-    trigger('collapseText2',[
+    trigger('collapseText2', [
       transition(':leave', [
-        animate('100ms', style({transform: 'scale(0)'}))
+        animate('100ms', style({ transform: 'scale(0)' }))
       ]),
       transition(':enter', [
-        style({transform: 'scale(0)'}),
-        animate('300ms 300ms', style({transform: 'scale(1)'}))
+        style({ transform: 'scale(0)' }),
+        animate('300ms 300ms', style({ transform: 'scale(1)' }))
       ])
     ]),
     trigger('collapse', [
@@ -36,7 +47,7 @@ export interface Nav {
         width: '200px'
       })),
       state('close', style({
-        width: '60px',
+        width: '60px'
       })),
       transition('open => close', group([
         query('@collapseText', animateChild()),
@@ -51,44 +62,63 @@ export interface Nav {
         query('@collapseItem', animateChild()),
         query('@collapseItem2', animateChild()),
         animate('600ms ease-in-out')
-      ])),
+      ]))
     ])
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit, OnDestroy {
+  #destroy = new Subject();
   public navItems: Nav[] = [{
-      path: 'gra',
-      name: 'Rozpocznij gre',
-      active: false,
-      icon: 'play.svg'
-    },{
-      path: 'zasady',
-      name: 'Zasady',
-      active: false,
-      icon: 'rules.svg'
-    },{
-      path: 'statystyki',
-      name: 'Statystyki',
-      active: false,
-      icon: 'statistics.svg'
-    }];
+    path: 'gra',
+    name: 'Rozpocznij gre',
+    icon: 'play.svg'
+  }, {
+    path: 'zasady',
+    name: 'Zasady',
+    icon: 'rules.svg'
+  }, {
+    path: 'statystyki',
+    name: 'Statystyki',
+    icon: 'statistics.svg'
+  }];
 
   public isCollapsed = false;
-  public isToggled = 'open'
+  public isToggled = 'open';
 
-  constructor() { }
+  @ViewChildren(SidebarItemComponent, { read: SidebarItemComponent })
+  sidebarItems = new QueryList<SidebarItemComponent>();
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
-    
+    this.router.events.pipe(
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+      takeUntil(this.#destroy)
+    )
+      .subscribe((e) => {
+        this.sidebarItems
+          .map((item) => {
+            const isEqual = item.sidebarItem.path === e.url.split('/').pop();
+            if (isEqual) {
+              return item.isActive = true;
+            }
+            return item.isActive = false;
+          });
+
+      });
   }
 
-  onActive(event){
-    console.log(event)
+  ngAfterViewInit(): void {
+  }
+  ngOnDestroy(): void {
+    this.#destroy.next();
+    this.#destroy.complete();
   }
 
-  collapseSidebar(){
+  collapseSidebar() {
     this.isCollapsed = !this.isCollapsed;
-    this.isToggled = this.isCollapsed ? 'close' : 'open'
+    this.isToggled = this.isCollapsed ? 'close' : 'open';
   }
 }
