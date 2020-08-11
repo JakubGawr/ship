3eb5e177d63613d01defcdc75c285f790605365e
unknown <dvdx@onet.pl>
2020-08-11 21:18:50 +0200

    cr poprawa chuj wie czy dobrze jest zajebiscie

--- libs/frontend/rules/src/lib/containers/rules/rules.component.ts
+++ libs/frontend/rules/src/lib/containers/rules/rules.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { BoardFacade } from '../../../../../game/src/lib/+state/facade/facade';
 
+export interface RulesIcons{
+  path: string;
+  caption: string;
+}
+
 @Component({
   selector: 'ship-game-rules',
   templateUrl: './rules.component.html',
   styleUrls: ['./rules.component.scss']
 })
 export class RulesComponent implements OnInit {
+
+  public icons: RulesIcons[] = [{
+    path: 'miss.svg',
+    caption: 'pudło'
+  },{
+    path: 'hit.svg',
+    caption: 'trafiony'
+  },{
+    path: 'sink.svg',
+    caption: 'zatopiony'
+  }];
 
   constructor(private boardFacade: BoardFacade) { }
 
