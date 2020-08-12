472e72f06d2ee15d6a141d8af49ca11b0f7ac53d
jakub-gawronski <jakub.gawronski@instapage.com>
2020-08-13 00:25:09 +0200

    boxy zrobione

---
+++ libs/table/src/lib/directives/destroy-box.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DestroyBoxDirective } from './destroy-box.directive';
+
+describe('DestroyBoxDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DestroyBoxDirective();
+    expect(directive).toBeTruthy();
+  });
+});
